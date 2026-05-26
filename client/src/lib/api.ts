3cdaf2cd8620f29a33c6d2782c4eddc72310aec5
timo7755 <lovrec.timotej@gmail.com>
2026-05-26 15:38:21 +0200

    updated api

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 // Before every request, read the token from localStorage and attach it
