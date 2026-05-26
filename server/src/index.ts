2aaaddd3396cf8d6daad05008a2f6a6e2a9e6373
timo7755 <lovrec.timotej@gmail.com>
2026-05-26 15:07:40 +0200

    CORS origin fixed for ubuntu

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use("*", logger());
 app.use(
   "*",
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   }),
 );
